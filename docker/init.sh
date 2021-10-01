61fcd89d18549c6c46074d41a556ef6216456934
Guillermo <guillermo.paoletti@gmail.com>
2021-10-01 15:47:54 +0200

    changes for deployment

---
+++ docker/init.sh
@@ -0,0 +1,5 @@
+export COIN=EVMOS
+export ETHEREUM_JSONRPC_VARIANT=geth
+export ETHEREUM_JSONRPC_HTTP_URL=http://host.docker.internal:8545/
+export ETHEREUM_JSONRPC_WS_URL=ws://host.docker.internal:8546/
+make start
