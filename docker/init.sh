2d5637a73efc46270b4f483c0d6752acc1598f1c
Guillermo Paoletti <guillermo.paoletti@gmail.com>
2021-10-07 19:14:12 +0200

    Update init.sh

--- docker/init.sh
+++ docker/init.sh
@@ -2,5 +2,5 @@
 export COIN=EVMOS
 export ETHEREUM_JSONRPC_VARIANT=geth
 export ETHEREUM_JSONRPC_HTTP_URL=${HTTP_URL:-http://host.docker.internal:8545/}
-export ETHEREUM_JSONRPC_WS_URL=${HTTP_URL:-ws://host.docker.internal:8546/}
+export ETHEREUM_JSONRPC_WS_URL=${WS_URL:-ws://host.docker.internal:8546/}
 make start
